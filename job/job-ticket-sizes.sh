fe53bfa13883c74f17aa4a1b0a8b47eb91ad80c2
SukritTM <sukrittmani@gmail.com>
2026-05-27 02:05:45 -0400

    adjusting job

--- job/job-ticket-sizes.sh
+++ job/job-ticket-sizes.sh
@@ -13,4 +13,4 @@ P=(8 16 32 64 128 256 512 1024)
 nvidia-smi
 module load conda/latest
 conda activate torchlth
-python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets.py -e 50 -r 10 -p 0.2 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cpu
+python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets.py -e 50 -r 10 -p 0.073 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cpu
