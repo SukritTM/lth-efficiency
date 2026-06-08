9b5beae898a7297188ec27c775381d64cc19f3d3
SukritTM <sukrittmani@gmail.com>
2026-06-08 02:25:19 -0400

    test change

--- torch-stream-test.py
+++ torch-stream-test.py
@@ -66,97 +66,97 @@ def train_parallel(models: list[nn.Module], iterations):
         loss.backward()
         optimizer.step()
 
-elapsed_times = []
-for _ in tqdm(range(100)):
+# elapsed_times = []
+# for _ in tqdm(range(100)):
 
-  model1 = nn.Sequential(
-      nn.Linear(10, 100),
-      nn.Linear(100, 1000),
-      nn.Linear(1000, 1)
-  ).to('cuda')
+#   model1 = nn.Sequential(
+#       nn.Linear(10, 100),
+#       nn.Linear(100, 1000),
+#       nn.Linear(1000, 1)
+#   ).to('cuda')
 
-  model2 = nn.Sequential(
-      nn.Linear(10, 100),
-      nn.Linear(100, 1000),
-      nn.Linear(1000, 1)
-  ).to('cuda')
+#   model2 = nn.Sequential(
+#       nn.Linear(10, 100),
+#       nn.Linear(100, 1000),
+#       nn.Linear(1000, 1)
+#   ).to('cuda')
 
-  model3 = nn.Sequential(
-      nn.Linear(10, 100),
-      nn.Linear(100, 1000),
-      nn.Linear(1000, 1)
-  ).to('cuda')
+#   model3 = nn.Sequential(
+#       nn.Linear(10, 100),
+#       nn.Linear(100, 1000),
+#       nn.Linear(1000, 1)
+#   ).to('cuda')
 
-  start_event = torch.cuda.Event(enable_timing=True)
-  end_event = torch.cuda.Event(enable_timing=True)
-  start_event.record()
+#   start_event = torch.cuda.Event(enable_timing=True)
+#   end_event = torch.cuda.Event(enable_timing=True)
+#   start_event.record()
 
-  train(model1, 1000)
-  train(model2, 1000)
-  train(model3, 1000)
+#   train(model1, 1000)
+#   train(model2, 1000)
+#   train(model3, 1000)
 
-  end_event.record()
-  torch.cuda.synchronize()  # Wait for the events to be recorded!
-  elapsed_time_ms = start_event.elapsed_time(end_event)
-  elapsed_times.append(elapsed_time_ms)
+#   end_event.record()
+#   torch.cuda.synchronize()  # Wait for the events to be recorded!
+#   elapsed_time_ms = start_event.elapsed_time(end_event)
+#   elapsed_times.append(elapsed_time_ms)
 
-print()
-print('Fully sequential:')
-print(np.mean(elapsed_times))
-print(np.std(elapsed_times))
+# print()
+# print('Fully sequential:')
+# print(np.mean(elapsed_times))
+# print(np.std(elapsed_times))
 
-elapsed_times = []
-for _ in tqdm(range(100)):
-  s1 = torch.cuda.Stream()
-  s2 = torch.cuda.Stream()
-  s3 = torch.cuda.Stream()
+# elapsed_times = []
+# for _ in tqdm(range(100)):
+#   s1 = torch.cuda.Stream()
+#   s2 = torch.cuda.Stream()
+#   s3 = torch.cuda.Stream()
 
-  model1 = nn.Sequential(
-      nn.Linear(10, 100),
-      nn.Linear(100, 1000),
-      nn.Linear(1000, 1)
-  ).to('cuda')
+#   model1 = nn.Sequential(
+#       nn.Linear(10, 100),
+#       nn.Linear(100, 1000),
+#       nn.Linear(1000, 1)
+#   ).to('cuda')
 
-  model2 = nn.Sequential(
-      nn.Linear(10, 100),
-      nn.Linear(100, 1000),
-      nn.Linear(1000, 1)
-  ).to('cuda')
+#   model2 = nn.Sequential(
+#       nn.Linear(10, 100),
+#       nn.Linear(100, 1000),
+#       nn.Linear(1000, 1)
+#   ).to('cuda')
 
-  model3 = nn.Sequential(
-      nn.Linear(10, 100),
-      nn.Linear(100, 1000),
-      nn.Linear(1000, 1)
-  ).to('cuda')
+#   model3 = nn.Sequential(
+#       nn.Linear(10, 100),
+#       nn.Linear(100, 1000),
+#       nn.Linear(1000, 1)
+#   ).to('cuda')
 
-  torch.cuda.synchronize()
+#   torch.cuda.synchronize()
 
-  start_event = torch.cuda.Event(enable_timing=True)
-  end_event = torch.cuda.Event(enable_timing=True)
-  start_event.record()
+#   start_event = torch.cuda.Event(enable_timing=True)
+#   end_event = torch.cuda.Event(enable_timing=True)
+#   start_event.record()
 
-  with torch.cuda.stream(s1):
-    train(model1, 1000)
+#   with torch.cuda.stream(s1):
+#     train(model1, 1000)
 
-  with torch.cuda.stream(s2):
-    train(model2, 1000)
+#   with torch.cuda.stream(s2):
+#     train(model2, 1000)
 
-  with torch.cuda.stream(s3):
-    train(model3, 1000)
+#   with torch.cuda.stream(s3):
+#     train(model3, 1000)
 
-  end_event.record()
-  torch.cuda.synchronize()  # Wait for the events to be recorded!
-  elapsed_time_ms = start_event.elapsed_time(end_event)
-  elapsed_times.append(elapsed_time_ms)
+#   end_event.record()
+#   torch.cuda.synchronize()  # Wait for the events to be recorded!
+#   elapsed_time_ms = start_event.elapsed_time(end_event)
+#   elapsed_times.append(elapsed_time_ms)
 
-  del s1
-  del s2
-  del s3
+#   del s1
+#   del s2
+#   del s3
 
-print()
-print('Coarse parallel:')
-print(np.mean(elapsed_times))
-print(np.std(elapsed_times))
+# print()
+# print('Coarse parallel:')
+# print(np.mean(elapsed_times))
+# print(np.std(elapsed_times))
 
 elapsed_times = []
 for _ in tqdm(range(100)):
