a2c2d2779b63c40460afcef8e07eee91649b2164
SukritTM <sukrittmani@gmail.com>
2026-06-11 17:28:40 -0400

    everything is cuda

--- job/job-ticket-sizes3.sh
+++ job/job-ticket-sizes3.sh
@@ -13,4 +13,4 @@ P=(10 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300)
 nvidia-smi
 module load conda/latest
 conda activate torchlth
-python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets.py -e 50 -r 10 -p 0.2000 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cpu
+python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets.py -e 50 -r 10 -p 0.2000 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cuda
