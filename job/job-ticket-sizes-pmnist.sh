c4f3a3bdb36ed596314cd6d259f3bae18bf3fb89
SukritTM <sukrittmani@gmail.com>
2026-06-13 19:41:53 -0400

    pmnist job adjustment

--- job/job-ticket-sizes-pmnist.sh
+++ job/job-ticket-sizes-pmnist.sh
@@ -13,4 +13,4 @@ P=(8 16 32 64 128 256 512 1024)
 nvidia-smi
 module load conda/latest
 conda activate torchlth
-python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets-pmnist.py -e 50 -r 10 -p 0.073 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cuda
+python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets-pmnist.py -e 50 -r 10 -p 0.2000 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cuda
