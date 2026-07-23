6a7a81d06aa14c462e1ff219dd2d7af75a409460
SukritTM <sukrittmani@gmail.com>
2026-07-22 22:55:44 -0400

    job updates

---
+++ job/job-l1-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -c 4
+#SBATCH --mem 32767
+#SBATCH -p gpu
+#SBATCH -G 1
+#SBATCH --constraint=l40s
+#SBATCH -t 10:00:00
+#SBATCH -o logs/results-%j.out
+
+nvidia-smi
+module load conda/latest
+conda activate torchlth
+python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/l1-models.py -e 500 -t 15 -s 30 -d cuda
