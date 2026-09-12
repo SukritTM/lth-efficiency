19a6c902f489f7fc4840c8744c1e155f956be0a2
SukritTM <sukrittmani@gmail.com>
2026-09-12 16:16:01 -0400

    ticket job

--- job/job-ticket-sizes.sh
+++ job/job-ticket-sizes.sh
@@ -6,11 +6,11 @@
 #SBATCH --constraint=l40s
 #SBATCH -t 15:00:00
 #SBATCH -o logs/results-%A-%a.out
-#SBATCH --array=0-7
+#SBATCH --array=0-8
 
-P=(8 16 32 64 128 256 512 1024)
+P=(8 16 32 64 128 256 300 512 1024)
 
 nvidia-smi
 module load conda/latest
 conda activate torchlth
-python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets.py -e 50 -r 10 -p 0.073 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cuda
+python /work/pi_jensen_umass_edu/sthiagarajam_umass_edu/lth-reimp/lth-efficiency/lottery-find-tickets.py -e 50 -r 10 -p 0.2500 -t 15 -s ${P[$SLURM_ARRAY_TASK_ID]} -d cuda
